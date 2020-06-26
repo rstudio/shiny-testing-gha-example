4bcc0e444ad8e5ca01c358b6b23eed7c5b53b734
Barret Schloerke <schloerke@gmail.com>
2020-06-26 11:28:59 -0400

    add a suffix using session info. Add mac, windows, linux folders

--- tests/shinytest.R
+++ tests/shinytest.R
@@ -1,4 +1,8 @@
 library(shinytest)
 shinytest::expect_pass(
-  shinytest::testApp("../", interactive = FALSE)
+  shinytest::testApp(
+    "../",
+    interactive = FALSE,
+    suffix = strsplit(sessioninfo::os_name(), " ")[[1]][1]
+  )
 )
