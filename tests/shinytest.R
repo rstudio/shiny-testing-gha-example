7041eaa1977e7455341f50768824009810d1bf8f
Barret Schloerke <schloerke@gmail.com>
2020-07-08 12:01:20 -0400

    fix unmerged file

--- tests/shinytest.R
+++ tests/shinytest.R
@@ -1,9 +1,5 @@
 library(shinytest)
-<<<<<<< HEAD
 expect_pass(testApp(
   "../",
   suffix = strsplit(utils::osVersion, " ")[[1]][1]
 ))
-=======
-expect_pass(testApp("../", compareImages = grepl("^macOS", utils::osVersion)))
->>>>>>> single_platform_snapshot
